4781dd2281026d7ba24591dc2697f52bc6ca5fe0
Coding-Abdi <abdi.brum@gmail.com>
2024-07-24 12:53:10 +0100

    created outline of counter app

---
+++ counter_app/index.js
@@ -0,0 +1,21 @@
+// intialize the count as 0
+// listen for clicks on the increment button
+// increment the count variable when the button is clicked
+// change the count-el in the HTML to reflect the new count
+
+// document.getElementById("count-el").innerText = count;
+
+let count = 0;
+
+let countEl = document.getElementById("count-el")
+
+function increment() {
+    count = count + 1;
+    countEl.innerText = count;
+}
+
+// 1. Create a function, save(), which logs out the count when it's called
+
+function save() {
+    console.log(count);
+}
